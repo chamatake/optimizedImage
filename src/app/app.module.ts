f753a9ad3b571f515f1a8d6f2945d371fdc4a94b
chamatake <chris@herodevs.com>
2022-09-06 13:00:05 -0600

    add content that was lost from stackblitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
+import { NgOptimizedImage } from '@angular/common';
+import { OldContentComponent } from './old-content/old-content.component';
+import { NewContentComponent } from './new-content/new-content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OldContentComponent,
+    NewContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule, BrowserModule, MatTabsModule, NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/old-content/old-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-old-content',
+  templateUrl: './old-content.component.html',
+  styleUrls: ['./old-content.component.css']
+})
+export class OldContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
